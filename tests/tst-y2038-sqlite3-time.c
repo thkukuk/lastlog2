da5d899c6068c64315f55f0743802db3dc590d3c
Thorsten Kukuk <kukuk@suse.com>
2023-03-14 12:16:23 +0100

    Add test for Y2038 safe storage of time in sqlite3

---
+++ tests/tst-y2038-sqlite3-time.c
@@ -0,0 +1,84 @@
+/* SPDX-License-Identifier: BSD-2-Clause
+
+  Copyright (c) 2023, Thorsten Kukuk <kukuk@suse.com>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* Test case:
+   Create an entry with an INT64_MAX-1000 timestamp, store that,
+   read that again and make sure the timestamp is correct.
+*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "lastlog2.h"
+
+#define BIG_TIME_VALUE (INT64_MAX - 1000)
+
+int
+main(void)
+{
+  const char *db_path = "y2038-sqlite3-time.db";
+  const char *user = "y2038";
+  time_t ll_time = 0;
+  char *error = NULL;
+
+  printf ("Big time value is: %ld\n", BIG_TIME_VALUE);
+
+  if (ll2_write_entry (db_path, user, BIG_TIME_VALUE, NULL, NULL, &error) != 0)
+    {
+      if (error)
+        {
+          fprintf (stderr, "%s\n", error);
+          free (error);
+        }
+      else
+	fprintf (stderr, "ll2_write_entry failed\n");
+      return 1;
+    }
+
+  if (ll2_read_entry (db_path, user, &ll_time, NULL, NULL, &error) != 0)
+    {
+      if (error)
+        {
+          fprintf (stderr, "%s\n", error);
+          free (error);
+        }
+      else
+        fprintf (stderr, "Unknown error reading database %s", db_path);
+      return 1;
+    }
+
+  if (ll_time != BIG_TIME_VALUE)
+    {
+      fprintf (stderr, "write/read entry time mismatch: written: %ld, got: %ld\n",
+	       BIG_TIME_VALUE, ll_time);
+      return 1;
+    }
+
+  return 0;
+}
